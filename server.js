9aa7a7a8c5f5aedadc59fac80d14fa94179b240d
Bruno Costa <obrnocsta@gmail.com>
2026-03-10 17:53:24 -0300

    feat: adiciona resposta para rota 'not found'

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 import http from "node:http";
 import { getData } from "./database/getData.js";
+import { json } from "node:stream/consumers";
 
 const PORT = 8000;
 
 const server = http.createServer(async (req, res) => {
   const data = await getData();
   if (req.url === "/api" && req.method === "GET") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(data));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        error: "not found",
+        message: "The requested route does not exist",
+      }),
+    );
   }
 });
 
